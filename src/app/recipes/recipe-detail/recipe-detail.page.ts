871014c4464a1d0d7f379277c212b9eec323855e
krishna saparia <krishnasaparia43045@gmail.com>
2021-03-21 00:00:50 -0400

    Add Dellete Recipe

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,32 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.page.html',
   styleUrls: ['./recipe-detail.page.scss'],
 })
 export class RecipeDetailPage implements OnInit {
+  loading = true;
   recipe: Recipe;
-  constructor( private recipeService: RecipesService, private activatedRoute: ActivatedRoute) { }
+  constructor( private recipeService: RecipesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router,
+               private alertController: AlertController) { }
 
   ngOnInit() {
     // params map vs snapshot
     this.activatedRoute.paramMap.subscribe(params => {
       if (!params.has('recipeId')){
         // rediret and return
-        return;
+        this.router.navigate(['/recipes']);
+        return ;
       }
       else{
         this.recipe = this.recipeService.getRecipe(params.get('recipeId'));
+        this.loading = false;
       }
 
-    }
-       );
+    });
+  }
+  onDeleteRecipe() {
+    this.alertController.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancle'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.recipeService.deleteRecipe(this.recipe.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
 
   }
-
 
 
 }
